10b7bd61b709d73925abaac19778b0a5ed6db2b1
unknown <milojacquet@gmail.com>
2024-03-24 17:42:21 -0700

    Change names

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() -> eframe::Result<()> {
     eframe::run_native(
         "eframe template",
         native_options,
-        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(hsc2_color_picker::TemplateApp::new(cc))),
     )
 }
 
@@ -37,7 +37,7 @@ fn main() {
             .start(
                 "the_canvas_id", // hardcode it
                 web_options,
-                Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+                Box::new(|cc| Box::new(hsc2_color_picker::TemplateApp::new(cc))),
             )
             .await
             .expect("failed to start eframe");
